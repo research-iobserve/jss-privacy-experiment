7ba8ecc3534739cf946c4752af80e87c9a86c6dd
Marc Adolf <mad@informatik.uni-kiel.de>
2018-07-19 14:18:42 +0200

    removed usa jpetstore from script

--- tag-and-push-to-repository.sh
+++ tag-and-push-to-repository.sh
@@ -9,7 +9,7 @@ else
 	exit 1
 fi
 
-DOCKER_IMAGES="jpetstore-usa-account-service jpetstore-order-service jpetstore-frontend-service jpetstore-catalog-service jpetstore-account-service"
+DOCKER_IMAGES="jpetstore-order-service jpetstore-frontend-service jpetstore-catalog-service jpetstore-account-service"
 
 for I in $DOCKER_IMAGES ; do
 	docker tag $I $DOCKER_REPO/$I
