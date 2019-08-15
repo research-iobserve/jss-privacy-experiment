61dceff5f57cc8297f8533336534bed4b406c253
Reiner Jung <reiner.jung@email.uni-kiel.de>
2019-08-15 15:27:15 +0200

    minor mods on node1

--- execute-accounting-ac-effector-impact.sh
+++ execute-accounting-ac-effector-impact.sh
@@ -11,6 +11,18 @@ fi
 
 . $BASE_DIR/common-functions.sh
 
+REPETITIONS=100000
+COUNT=1000
+
+if [ "$1" == "" ] ; then
+	RUN_TYPE="accounting-sans"
+	CONTAINER="jpetstore-account-service-sans-effector"
+	LIST_SIZE="NONE"
+else
+	RUN_TYPE="accounting/$1"
+	CONTAINER="jpetstore-account-service"
+	LIST_SIZE="$1"
+fi
 
 #############################################
 # common functions
@@ -33,7 +45,7 @@ function stopDocker() {
 function startDocker() {
 	information "Start accounting component ..."
 
-	docker run -e LOGGER=$LOGGER -e LOCATION=GERMANY -d --name account jpetstore-account-service
+	docker run -e LOGGER=$LOGGER -e LOCATION=GERMANY -d --name account $CONTAINER
 
 	ID=`docker ps | grep 'account' | awk '{ print $1 }'`
 	ACCOUNT_SERVICE=`docker inspect $ID | grep '"IPAddress' | awk '{ print $2 }' | tail -1 | sed 's/^"\(.*\)",/\1/g'`
@@ -92,7 +104,7 @@ kieker.monitoring.writer.filesystem.BinaryFileWriter.compression=kieker.monitori
 EOF
 
         export DRIVE_ACCOUNTING_OPTS="-Dlog4j.configuration=file:///${BASE_DIR}/log4j.cfg -Dkieker.monitoring.configuration=${ACCOUNTING_KIEKER_PROPERTIES}"
-	${DRIVE_ACCOUNTING} -u $1 -d 100 -c 1000 -r 100000
+	${DRIVE_ACCOUNTING} -u $1 -d 100 -c $COUNT -r $REPETITIONS
 }
 
 # run collector
@@ -145,12 +157,38 @@ function stopCollector() {
 }
 
 # configure effector
+# $1 = list size
 function configureEffector() {
 	information "Running effector configuration"
-	RUNTIME_RECONFIGURE_MONITORING_CONTROLLER_OPTS="-Dlog4j.configuration=file:///${BASE_DIR}/log4j.cfg"
-	${AC_CONFIGURATION} -h "${ACCOUNT_SERVICE}" -p 5791 -ws 10.0.0.1 -we 10.0.39.21 -bs 10.1.0.1 -be 10.0.39.21 -w 172.17.0.1 172.17.0.1
+
+	PREFIX="10.10"
+
+	B_BASE="1"
+	W_BASE=`expr $1 + 10`
+
+        BS_LOW=`expr $B_BASE % 256`
+        BS_HIGH=`expr $B_BASE / 256`
+
+        BE=`expr $B_BASE + $1`
+        BE_LOW=`expr $BE % 256`
+        BE_HIGH=`expr $BE / 256`
+
+        WS_LOW=`expr $W_BASE % 256`
+        WS_HIGH=`expr $W_BASE / 256`
+
+        WE=`expr $W_BASE + $1`
+        WE_LOW=`expr $WE % 256`
+        WE_HIGH=`expr $WE / 256`
+
+        information "blacklist $BS_HIGH.$BS_LOW - $BE_HIGH.$BE_LOW  whitelist $WS_HIGH.$WS_LOW - $WE_HIGH.$WE_LOW"
+
+        export RUNTIME_RECONFIGURE_MONITORING_CONTROLLER_OPTS="-Dlog4j.configuration=file:///$BASE_DIR/log4j.cfg -Dkieker.monitoring.configuration=$CONTROL_TIME_PROPERTIES"
+        $AC_CONFIGURATION -bs "$PREFIX.$BS_HIGH.$BS_LOW" -be "$PREFIX.$BE_HIGH.$BE_LOW" \
+                -h "${ACCOUNT_SERVICE}" -p 5791 -ws "$PREFIX.$WS_HIGH.$WS_LOW" -we "$PREFIX.$WE_HIGH.$WE_LOW" -w 172.17.0.1 "${ACCOUNT_SERVICE}"
+
 	information "done"
 }
+
 
 ###################################
 # check parameters
@@ -158,8 +196,13 @@ function configureEffector() {
 ACCOUNTING_KIEKER_PROPERTIES="$BASE_DIR/kieker-drive-accounting.properties"
 COLLECTOR_PROPERTIES="$BASE_DIR/kieker-collector.properties"
 
-export STORAGE_PATH="${DATA_DIR}/accounting/responses"
-export COLLECTOR_DATA_PATH="${DATA_DIR}/accounting/collector"
+export STORAGE_PATH="${DATA_DIR}/${RUN_TYPE}/responses"
+export COLLECTOR_DATA_PATH="${DATA_DIR}/${RUN_TYPE}/collector"
+
+checkExecutable access-control "$AC_CONFIGURATION"
+checkExecutable collector "$COLLECTOR"
+checkExecutable drive-accounting "$DRIVE_ACCOUNTING"
+checkDirectory data-dir "${DATA_DIR}"
 
 if [ -d "${STORAGE_PATH}" ] ; then
 	rm -rf "${STORAGE_PATH}"
@@ -172,12 +215,6 @@ mkdir -p "${STORAGE_PATH}"
 mkdir -p "${COLLECTOR_DATA_PATH}"
 
 ###################################
-# check tools
-checkExecutable access-control "$AC_CONFIGURATION"
-checkExecutable collector "$COLLECTOR"
-checkExecutable drive-accounting "$DRIVE_ACCOUNTING"
-
-###################################
 # check if no leftovers are running
 
 # stop docker
@@ -186,13 +223,17 @@ stopDocker
 ###################################
 # run experiment
 
-startCollector 
+startCollector
 
 sleep 10
 
 startDocker
 
-configureEffector
+if [ "${LIST_SIZE}" != "NONE" ] ; then
+	configureEffector "${LIST_SIZE}"
+else
+	information "no effector"
+fi
 
 driveService "${SERVICE_URL}"
 
